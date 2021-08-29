92247c475f4a746dc3df11911f3a3160d7d56703
tomgodee <trungvu8295@gmail.com>
2021-08-29 16:36:12 +0700

    Update

--- app/services/DatabaseService.js
+++ app/services/DatabaseService.js
@@ -1,11 +1,11 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(`postgres://postgres:zxc321@${process.env.DB_ADDRESS}:5432/stock_market`);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 // const sequelize = new Sequelize('postgres://postgres:zxc321@db:5432/stock_market');
 
 sequelize.authenticate().then(() => {
   console.log('Connection has been established successfully.');
-}).catch(() => {
+}).catch((error) => {
   console.error('Unable to connect to the database:', error);
 });
 
